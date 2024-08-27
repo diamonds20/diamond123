5da7d76dbbd5ff688db3b0bf52047e54b40e52cd
RizaRizvi <rizarizvi2@gmail.com>
2024-08-27 13:18:42 +0530

    change

--- karma.conf.js
+++ karma.conf.js
@@ -37,11 +37,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['Chrome', 'ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', '--disable-gpu']
       }
     },
     singleRun: false,
